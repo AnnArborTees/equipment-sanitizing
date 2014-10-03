e9e5ce85ee1b0ed2a8f47a3a86a519968e96593d
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-10-02 21:46:44 -0400

    no migrations

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, '/home/ubuntu/RailsApps/www.annarborequipmentsanitizing.com'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml config/paypal.yml config/authorize_net.yml config/smtp.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
